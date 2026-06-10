ed591dde1ce8cb3e2a406992a64fcc0be5d2cd51
edp312jambi-alt <edp312jambi@gmail.com>
2026-06-10 18:12:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.write("Akses publik cepat untuk melihat ratusan ribu data langsung dari cloud
 
 # 2. Konfigurasi Endpoint Proyek Supabase Baru Anda
 SUPABASE_URL = "https://smiepesiidolrcztrboq.supabase.co"
-SUPABASE_KEY = "Matahari3123450"  # <-- Ganti dengan anon key baru Anda
+SUPABASE_KEY = "smiepesiidolrcztrboq"  # <-- Ganti dengan anon key baru Anda
 
 # 3. Fungsi Pengambil Data Otomatis (Pagination)
 @st.cache_data(ttl=600)
